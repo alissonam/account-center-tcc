5dc73996ff410ceae6ab85d4f7cbe98084cb27c6
William_Kolina <william_kolina@pop-os.localdomain>
2022-07-08 17:31:42 -0300

    Create module of product

---
+++ backend/database/migrations/2022_07_08_141513_crate_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('status', ['active', 'inative'])->default('active');
+            $table->string('code')->nullable();
+            $table->string('action_url')->nullable();
+            $table->string('app_url')->nullable();
+            $table->string('api_token')->nullable();
+            $table->text('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
