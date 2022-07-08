b437a32936ec132ed287de0b6d1887f0a28e0131
alissonam <alissonalmachado@gmail.com>
2022-07-08 14:26:05 -0300

    Adjust

--- backend/database/migrations/2022_07_08_110314_create_plans_table.php
+++ backend/database/migrations/2022_07_08_110314_create_plans_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
             $table->string('name');
             $table->json('payload');
             $table->text('description');
             $table->boolean('hidden')->default(false);
             $table->boolean('preferential')->default(false);
-            $table->unsignedBigInteger('product_id');
             $table->timestamps();
         });
     }
