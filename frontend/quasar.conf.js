8334592e3429c7f348fd6a8ac4b22da254bbdf55
Fabio <fabiojd47@gmail.com>
2022-07-20 11:42:05 -0300

    Container port

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -83,7 +83,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: process.env.DEV_SERVER_PORT,
       open: false // opens browser window automatically
     },
 
